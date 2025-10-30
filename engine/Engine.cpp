1b96f61cd60ef879a08a2607ff8b024d94575040
Do The Anh <dta.business@gmail.com>
2025-10-31 01:14:30 +0700

    Adjust macro break codes

--- engine/Engine.cpp
+++ engine/Engine.cpp
@@ -34,8 +34,8 @@ static vector<Uint8> _macroBreakCode = {
     KEY_RETURN, KEY_SPACE, KEY_TAB,
     // Numbers (Shift + number = !, @, #, $, %, ^, &, *, (, ))
     KEY_1, KEY_2, KEY_3, KEY_4, KEY_5, KEY_6, KEY_7, KEY_8, KEY_9, KEY_0,
-    // Punctuation and symbols (, . ; ' [ ])
-    KEY_COMMA, KEY_DOT, KEY_SEMICOLON, KEY_QUOTE,
+    // Punctuation và ký hiệu (, . / ; ' [ ])
+    KEY_COMMA, KEY_DOT, KEY_SLASH, KEY_SEMICOLON, KEY_QUOTE,
     KEY_LEFT_BRACKET, KEY_RIGHT_BRACKET
 };
 
